1603b716463eba0a5afe6ae4f167d5a1aa4cb9ff
Ella C <107303409+ella-emc@users.noreply.github.com>
2023-10-09 02:34:14 +0800

    Initial commit

---
+++ lottery_craze.py
@@ -0,0 +1,28 @@
+# Ask for the winning combination from the user
+# Read the numbers from the file
+# If the entry from the file matches 6 numbers, write it to another file
+# If the entry from the file matches 5 numbers, write it to another file
+# If the entry from the file matches 4 numbers, write it to another file
+# If the entry from the file matches 3 numbers, write it to another file
+
+# Count the number of winning entries in 6, 5, 4, and 3 combinations
+# Display the amount of prize for each and the total amount of prize in each number category
+import sys
+
+def main():
+    display_home()
+    combination = input("Enter today's 6-number winning combination: ")
+    
+    while True:
+        try:
+
+    
+
+
+def display_home():
+    print("**************************************************")
+    print()
+
+
+if __name__ == "__main__":
+    main()
